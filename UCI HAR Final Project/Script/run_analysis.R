9455742d1412515026f36b99f083e8d76b878be6
JP Monteagudo <100309883+jpmonteagudo28@users.noreply.github.com>
2023-06-20 20:46:58 -0400

    Updating code with clean, joined data set. Still missing tidy data set composed of acitivities' averages.

--- UCI HAR Final Project/Script/run_analysis.R
+++ UCI HAR Final Project/Script/run_analysis.R
@@ -1,5 +1,5 @@
 ##################################################################
-# 1. Get and unzip data. Load packages needed for analysis
+# A. Get and unzip data. Load packages needed for analysis
 ##################################################################
 
 unzip("getdata_projectfiles_UCI HAR Dataset.zip",exdir = getwd())
@@ -7,10 +7,77 @@ my_packages <- c("dplyr","data.table","Hmisc")
 lapply(my_packages,require,character.only=TRUE)
 
 ##################################################################
-# 2. Read the test and training data
+# B. Read the test and training data & variable labels
 ##################################################################
 
-pathto_file <- "./UCI HAR Dataset"
+pathto_file <- "./UCI HAR Final Project/UCI HAR Dataset"
 test_subjects <- read.table(file.path(pathto_file,"test","subject_test.txt"), header = FALSE, sep = " ")
-test_results <- read.table(file.path(pathto_file,"test","X_test.txt"), header = FALSE, sep = " ", fill = TRUE)
+
+## if you're using Windows' notepad, you will get undelimited X_test.txt and X_train.txt files 
+## so recommend defining " " as na.strings
+
+test_results <- read.table(file.path(pathto_file,"test","X_test.txt"), header = FALSE, na.strings = " ")
 test_activities <- read.table(file.path(pathto_file,"test","y_test.txt"), header = FALSE, sep = " ")
+
+## Features data
+
+features <- read.table(file.path(pathto_file,"text_Data","features.txt"), header = FALSE, sep = " ")
+features <-features[,2]
+features <- gsub("[\\(\\)-]","",features)
+features <- gsub("^t","time_",features)
+features <- gsub("^f","freq_",features)
+features <- gsub("BodyBody","body",features)
+features <- gsub("Mag","magnitude",features)
+features <- gsub("Acc","_Accelerometer_",features)
+features <- gsub("Gyro", "_Gyroscope_",features)
+features <- gsub("std","_StDev.",features)
+features <- gsub("mean","_Mean.",features)
+features <- gsub("anglet","angle_time_",features)
+
+
+## Activities
+
+activities <- read.table(file.path(pathto_file,"text_Data","activity_labels.txt"), header = FALSE, sep = " ")
+colnames(activities) <- c("ID","Label")
+
+## Train data
+
+training_subjects <-read.table(file.path(pathto_file,"train","subject_train.txt"), header = FALSE, sep = " ")
+training_results <- read.table(file.path(pathto_file,"train","X_train.txt"), header = FALSE, na.strings = " ")
+training_activities <- read.table(file.path(pathto_file,"train","y_train.txt"), header = FALSE, sep = " ")
+
+
+####################################################################
+# Step 1 & 2. Combine data sets & extract mean & SD measurements
+####################################################################
+
+test_data <-cbind(test_subjects,test_activities,test_results)
+colnames(test_data) <- c("subject", "activities",features)
+cols_to_keep <-grep("subject|activities|Mean.[X-Z]|StDev.[X-Z]",colnames(test_data))
+test_data <- test_data[,cols_to_keep]
+
+## Train data
+
+train_data <- cbind(training_subjects,training_activities, training_results)
+colnames(train_data) <- c("subject","activities",features)
+cols_to_keep2 <-grep("subject|activities|Mean.[X-Z]|StDev.[X-Z]",colnames(train_data))
+train_data <- train_data[,cols_to_keep2]
+
+## Joining both data sets
+
+humanAR_df <- rbind(test_data,train_data)
+
+## Remove unnecessary data tables
+
+rm(test_activities,test_results, test_subjects,training_activities,training_results,training_subjects)
+
+#####################################################################
+# Step 3. Descriptive activity names
+#####################################################################
+
+## Making humanAR$activities into factor var. with 6 levels
+
+humanAR_df$activities <- factor(humanAR_df$activities, levels = activities[,1], labels = activities[,2])
+
+
+
